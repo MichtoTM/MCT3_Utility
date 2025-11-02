1488f45d27c4503f00bcc44aa0d4c47e7eb0b1ea
MichtoTM <carltroismoulins@outlook.fr>
2025-11-02 16:33:25 +0100

    modifiacation du setup

--- setup.py
+++ setup.py
@@ -3,15 +3,19 @@ from setuptools import setup, find_packages
 setup(
     name="mct3",
     version="2.0.3",
+    author="MichtOo",
+    description="CLI tool for audio and file utilities.",
     packages=find_packages(),
+    python_requires=">=3.10",
     install_requires=[
-        "mutagen",
-        "click",
-        "requests",
+        "click>=8.3",
+        "mutagen>=1.47.0",
+        "requests>=2.32.5",
     ],
     entry_points={
-        'console_scripts': [
-            'mct3=mct3.main:cli',
+        "console_scripts": [
+            "mct3=mct3.main:cli",
         ],
     },
+    include_package_data=True,
 )
